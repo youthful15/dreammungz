7301dce6a8c3edf49add3604d5af8a550a0dec75
안지영 <anji6978@naver.com>
2022-09-07 17:51:56 +0900

    Chore: Tailwind css 설치

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold">드림멍즈 프로젝트 세팅</h1>
+      <h2 className="text-2xl font-bold">타입스크립트</h2>
+      <h2 className="text-2xl font-bold">Tailwind</h2>
+    </div>
+  )
+}
+
+export default App
