ed12ecc706db860f77dc60e6ac0e19fb986739aa
박종민 <qooktree@naver.com>
2022-09-13 18:10:28 +0900

    Fix: webpack 충돌 해결 1

---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,24 @@
+export default function MainPage() {
+
+  return <div className="w-full">
+    <div>
+    <p>드림멍즈가 처음이신가요?</p>
+    <p>튜토리얼 바로가기</p>
+    </div>
+
+    <main>
+      새롭게 추가된 무지개 강아지를 만나 보세요~~
+    </main>
+
+    <aside className="flex">
+      <div>
+        취뽀 강아지
+      </div>
+
+      <div>
+        공략 게시판
+      </div>
+    </aside>
+
+  </div>
+}
