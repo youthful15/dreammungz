f28cab856c089fd7491b16efebfc84a9b5a623a0
songji <youthful_15@naver.com>
2022-09-15 17:40:30 +0900

    Feat: 게임 진행 페이지 status 받기 추가

--- frontend/src/pages/GamePlaying.tsx
+++ frontend/src/pages/GamePlaying.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import findKOR from "../utils/findKOR"
 
 const story = {
@@ -21,25 +22,65 @@ const story = {
   ],
 }
 
+function Information() {
+  return (
+    <div className="h-full">
+      <div>아무튼 게임스러운 디자인</div>
+      <div>더 넣을게 뭐가 있을까</div>
+      <div>
+        귀여움
+        <progress max="20" value="20" className="html5"></progress>
+      </div>
+      <div>
+        정의로움
+        {story.justice}
+      </div>
+      {story.status.map(({ name, value }, index) => (
+        <div className="flex w-full">
+          <span className="w-1/5">{findKOR(name)}</span>
+          <progress max="20" value={value} className="w-4/5 html5"></progress>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function Game() {
+  const choiceSelect = (index: number) =>
+    alert(index + 1 + "번 선택지를 고르셨습니다.")
+
+  return (
+    <div className="h-full">
+      <div className="h-[10%]">{story.title}</div>
+      <div className="h-[90%] flex flex-col justify-between">
+        <div>
+          <img src={story.image} alt="" />
+          <div>{story.content}</div>
+        </div>
+        <div className="px-2">
+          {story.selection.map((select, index) => (
+            <button
+              className="my-2 py-2 w-full bg-purple-100"
+              key={index}
+              onClick={() => choiceSelect((index = index))}
+            >
+              {select}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function GamePlaying() {
   return (
     <div className="w-full h-full flex">
-      <div className="w-1/2 h-full bg-blue-100">{findKOR("SENSIBILITY")}</div>
-      <div className="w-1/2 h-full bg-green-100">
-        <div className="h-[10%]">{story.title}</div>
-        <div className="h-[90%] flex flex-col justify-between">
-          <div>
-            <img src={story.image} alt="" />
-            <div>{story.content}</div>
-          </div>
-          <div className="px-2">
-            {story.selection.map((select, index) => (
-              <div className="my-2 py-2 w-full bg-purple-100" key={index}>
-                {select}
-              </div>
-            ))}
-          </div>
-        </div>
+      <div className="w-2/5 h-full bg-blue-100">
+        <Information />
+      </div>
+      <div className="w-3/5 h-full bg-green-100">
+        <Game />
       </div>
     </div>
   )
