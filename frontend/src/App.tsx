f505715e000069e071d48af12b0c995ae2208239
안지영 <anji6978@naver.com>
2022-09-16 18:33:35 +0900

    Feat:tailwind 커스텀 컬러 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
-import Sample from "./pages/Sample.js"
+import Sample from "./pages/Sample"
 import AllNftListPage from "./pages/AllNftListPage"
 import NftDetail from "./pages/NftDetail"
 import GameStart from "./pages/GameStart"
