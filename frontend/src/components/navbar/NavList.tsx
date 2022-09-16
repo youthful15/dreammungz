7f47635cf2cfcc3ec3b3f225cba232236189353e
안지영 <anji6978@naver.com>
2022-09-17 03:29:14 +0900

    Feat: 메인페이지 navbar 추가

--- frontend/src/components/navbar/NavList.tsx
+++ frontend/src/components/navbar/NavList.tsx
@@ -1,10 +1,10 @@
 import NavItem from "./NavItem"
 
 const navItemList = [
+  { title: "메인페이지", path: "/mainpage" },
   { title: "튜토리얼", path: "/tutorial" },
   { title: "강아지 구경", path: "/nft/list" },
   { title: "커뮤니티", path: "/community" },
-  // { title: "메인페이지", path: "/mainpage" },
   { title: "디자인(개발용)", path: "/styles" },
 ]
 
