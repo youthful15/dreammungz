c0ecf72d4850e6152d1590906994f060ca558be7
안지영 <anji6978@naver.com>
2022-09-19 17:34:07 +0900

    Feat: 필터링 form 생성 및 url 쿼리 생성 추가 [S07P22A605-151]

--- frontend/src/pages/AllNftListPage.tsx
+++ frontend/src/pages/AllNftListPage.tsx
@@ -1,11 +1,32 @@
 import { useState } from "react"
 import Filter from "../components/filter/Filter"
 import NftList from "../components/nftList/NftList"
-
+import useQueryParam from "../components/filter/useQueryParam"
 const NftListPage = () => {
+  let [filter, setFilter] = useQueryParam<Filter>("search")
+  const [curPage, setPage] = useState(0)
+
+  if (!filter) {
+    filter = {
+      job: "null",
+      hair: "null",
+      tier: "null",
+      color: "null",
+      gender: "null",
+      face: "null",
+      sell: false,
+      status: [],
+      page: 0,
+    }
+  }
+
   return (
-    <div>
-      <Filter />
+    <div className="relative">
+      <pre className="absolute z-30 bg-white right-0">
+        {JSON.stringify(filter || {}, null, 1)}
+      </pre>
+
+      <Filter setFilter={setFilter} />
       <NftList type="test" />
     </div>
   )
