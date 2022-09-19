66270519523498234d772e8aa98e43f7cbb045e5
Songji <youthful_15@naver.com>
2022-09-19 17:04:13 +0900

    Feat: html2canvas로 canvas dataUrl 추출

--- frontend/src/pages/GameEnding.tsx
+++ frontend/src/pages/GameEnding.tsx
@@ -1,3 +1,6 @@
+import html2canvas from "html2canvas"
+import { useRef, useEffect } from "react"
+
 const NFT = {
   color: "PINK",
   hair: "CURLY",
@@ -19,30 +22,49 @@ const NFT = {
 }
 
 export default function GameEnding() {
+  const canvasRef = useRef(null)
+
+  const copyDOM = async () => {
+    window.scrollTo(0, 0)
+
+    let url = ""
+    await html2canvas(canvasRef.current!).then(async (canvas) => {
+      console.log("확인용", canvas.toDataURL("image/jpg"))
+      url = await canvas.toDataURL("image/jpg").split(",")[1]
+      // console.log("여기왔나용", url)
+    })
+  }
+
+  useEffect(() => {
+    copyDOM()
+  }, [])
+
   return (
     <div className="w-full">
       여기엔딩
       <div className="relative">
-        <img
-          src={`/assets/bg/${NFT.job}.png`}
-          className="h-[400px] absolute top-0 left-0"
-        />
-        <img
-          src={`/assets/haircolor/${NFT.hair}_${NFT.color}.png`}
-          className="h-[400px] absolute top-0 left-0"
-        />
-        <img
-          src={`/assets/face/${NFT.face}.png`}
-          className="h-[400px] absolute top-0 left-0"
-        />
-        <img
-          src={`/assets/job/${NFT.job}.png`}
-          className="h-[400px] absolute top-0 left-0"
-        />
-        <img
-          src={`/assets/tier/${NFT.tier}.png`}
-          className="h-[400px] absolute top-0 left-0"
-        />
+        <div ref={canvasRef} className="h-[400px] w-[400px] bg-blue-200">
+          <img
+            src={`/assets/bg/${NFT.job}.png`}
+            className="h-[400px] absolute top-0 left-0"
+          />
+          <img
+            src={`/assets/haircolor/${NFT.hair}_${NFT.color}.png`}
+            className="h-[400px] absolute top-0 left-0"
+          />
+          <img
+            src={`/assets/face/${NFT.face}.png`}
+            className="h-[400px] absolute top-0 left-0"
+          />
+          <img
+            src={`/assets/job/${NFT.job}.png`}
+            className="h-[400px] absolute top-0 left-0"
+          />
+          <img
+            src={`/assets/tier/${NFT.tier}.png`}
+            className="h-[400px] absolute top-0 left-0"
+          />
+        </div>
       </div>
     </div>
   )
