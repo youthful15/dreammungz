af0d8986fa801d729138184f53167a38d9559976
박종민 <qooktree@naver.com>
2022-09-19 13:13:56 +0900

    Fix: Metamask 네트워크 추가 함수 제거 및 연결 함수 구현

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -2,11 +2,26 @@ import { useState } from "react"
 import { Link, NavLink } from "react-router-dom"
 import NavList from "./NavList"
 import Login from "../login/Login"
-
+import Web3 from "web3"
+import { ssafyContractAddress, ERC20ABI } from "../../utils/web3"
 const navItemStyle: string = "bg-brown-300  border rounded-lg shadow-sm"
 
 const Navbar = () => {
+  const web3: any = new Web3(window.ethereum)
+  const [showbalance, setShowBalance] = useState()
   const [isLogin, setLogin] = useState(false) // 추후 Recoil을 사용하여  상태관리 할 것
+  const checkBalance = () => {
+    web3.eth
+      .getBalance(localStorage.getItem("publicAddress"))
+      .then((res: any) => {
+        const token = new web3.eth.Contract(ERC20ABI, ssafyContractAddress)
+        console.log("token", token)
+        const balance = token.balanceOf(localStorage.getItem("publicAddress"))
+        console.log("res", res)
+        console.log("balance", balance)
+      })
+  }
+
   return (
     <nav className="flex flex-col justify-between w-full h-full p-2 space-y-4 text-center">
       <ul>
@@ -28,7 +43,9 @@ const Navbar = () => {
           <Link to="/mypage/list">
             <div className={navItemStyle}>마이페이지 </div>
           </Link>
-          <div className={navItemStyle}>지갑 정보 보기</div>
+          <div className={navItemStyle} onClick={() => checkBalance()}>
+            지갑 정보 보기
+          </div>
           <div className={navItemStyle} onClick={() => setLogin(false)}>
             로그아웃
           </div>
