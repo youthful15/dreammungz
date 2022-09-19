5d5d04f34b8e75c7868bf013989c174204b95392
Songji <youthful_15@naver.com>
2022-09-19 14:44:00 +0900

    Feat: assets 위치 변경 및 엔딩 레이어 쌓기 추가

--- frontend/src/pages/GameEnding.tsx
+++ frontend/src/pages/GameEnding.tsx
@@ -1,9 +1,9 @@
 const NFT = {
-  color: "BLACK",
-  hair: "ORIGINAL",
-  face: "SHY",
-  tier: "RARE",
-  job: "VOCALIST",
+  color: "PINK",
+  hair: "CURLY",
+  face: "BEAN",
+  tier: "EPIC",
+  job: "DOCTOR",
   status: [
     {
       name: "CUTE",
@@ -19,5 +19,31 @@ const NFT = {
 }
 
 export default function GameEnding() {
-  return <div className="w-full"></div>
+  return (
+    <div className="w-full">
+      여기엔딩
+      <div className="relative">
+        <img
+          src={`/assets/bg/${NFT.job}.png`}
+          className="h-[400px] absolute top-0 left-0"
+        />
+        <img
+          src={`/assets/haircolor/${NFT.hair}_${NFT.color}.png`}
+          className="h-[400px] absolute top-0 left-0"
+        />
+        <img
+          src={`/assets/face/${NFT.face}.png`}
+          className="h-[400px] absolute top-0 left-0"
+        />
+        <img
+          src={`/assets/job/${NFT.job}.png`}
+          className="h-[400px] absolute top-0 left-0"
+        />
+        <img
+          src={`/assets/tier/${NFT.tier}.png`}
+          className="h-[400px] absolute top-0 left-0"
+        />
+      </div>
+    </div>
+  )
 }
