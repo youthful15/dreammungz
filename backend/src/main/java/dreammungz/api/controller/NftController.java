7e6475bf92d26d36e7efa11ae994f02f53eaaf8f
seulgi <seulgi9834@naver.com>
2022-09-21 10:14:41 +0900

    Fix: Swagger 오류 해결

--- backend/src/main/java/dreammungz/api/controller/NftController.java
+++ backend/src/main/java/dreammungz/api/controller/NftController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
     */
 
 @RestController
-@RequestMapping("/api/nft")
+@RequestMapping("/nft")
 @RequiredArgsConstructor
 public class NftController {
     private final NftService nftService;
