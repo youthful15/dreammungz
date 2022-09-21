843bf927cb284c73cef1f0007d1749427e22bcd7
박종민 <qooktree@naver.com>
2022-09-21 09:00:14 +0900

    Feat: Modal Component 추가, NFTDetail 즉시 구매, 가격 제안, 판매 중지 틀 작성

--- frontend/src/pages/NftDetail.tsx
+++ frontend/src/pages/NftDetail.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
 import NftListItem2 from "../components/nftList/NftListItem2"
+import Modal from "../components/modal/Modal"
 
 interface TradeListProp {
   sellerNickname: string
@@ -79,12 +81,103 @@ const tradeList: any = [
 ]
 
 const NftDetail = () => {
-  const [myNft, setMyNft] = useState(false)
-  const [isSelling, setIsSelling] = useState(false)
-  const [clickedSell, setClickedSell] = useState(false)
+  const navigate = useNavigate()
+  const [myNft, setMyNft] = useState(false) // 본인 NFT 인지 확인
+  const [isSelling, setIsSelling] = useState(false) // 판매중인지 확인
+  const [clickedSell, setClickedSell] = useState(false) // 판매 눌렀는지 확인
 
+  const [isOpen1, setOpen1] = useState(false) // 판매 중지 모달
+  const modalClose1 = () => setOpen1(false) // 판매 중지 모달
+
+  const [isOpen2, setOpen2] = useState(false) // 즉시 구매 모달
+  const modalClose2 = () => setOpen2(false) // 즉시 구매 모달
+
+  const [isOpen3, setOpen3] = useState(false) // 가격 제안하기 모달
+  const modalClose3 = () => setOpen3(false) // 가격 제안하기 모달
+  const [proposal, setProposal] = useState<HTMLInputElement>() // 가격 제안 가격
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.currentTarget
+    setProposal(value)
+  }
+  console.log(proposal)
   return (
     <div className="h-full w-full">
+      {/* 판매 중지 모달 시작 */}
+      <Modal isOpen={isOpen1} modalClose={modalClose1}>
+        <p className="text-xl font-semibold mb-4">판매를 중지하시겠습니까?</p>
+
+        <div className="flex justify-center">
+          <button
+            className="mr-4 border border-black"
+            onClick={() => {
+              modalClose1()
+              setIsSelling(false)
+            }}
+          >
+            확인
+          </button>
+          <button className="border border-black" onClick={() => modalClose1()}>
+            닫기
+          </button>
+        </div>
+      </Modal>
+      {/* 판매 중지 모달 끝 */}
+
+      {/* 즉시 구매 모달 시작 */}
+      <Modal isOpen={isOpen2} modalClose={modalClose2}>
+        <p className="text-xl font-semibold mb-4">즉시 구매하시겠습니까?</p>
+        <p className="mb-4">즉시 구매 가격: 400</p>
+        <p>나의 MUNG: {100}MUNG</p>
+        <div className="flex justify-center">
+          <button
+            className="mr-4 border border-black"
+            onClick={() => {
+              modalClose2()
+              setIsSelling(false)
+              navigate("/nft/list") // 즉시 구매시 nft list page로 redirect
+            }}
+          >
+            구매
+          </button>
+          <button className="border border-black" onClick={() => modalClose2()}>
+            취소
+          </button>
+        </div>
+      </Modal>
+      {/* 즉시 구매 모달 끝 */}
+
+      {/* 가격 제안하기 모달 시작 */}
+      <Modal isOpen={isOpen3} modalClose={modalClose3}>
+        <p className="text-xl font-semibold mb-4">
+          얼만큼 가격을 제시하겠습니까?
+        </p>
+        <p className="mb-4">가격 제시</p>
+        <p>나의 지갑에 들어가 있는 Token 수: {0} MUNG</p>
+        <input
+          className="border border-black"
+          id="proposal"
+          type="text"
+          onChange={onChange}
+        />
+        <label htmlFor="proposal">MUNG</label>
+        <div className="flex justify-center">
+          <button
+            className="mr-4 border border-black"
+            onClick={() => {
+              modalClose3()
+              setIsSelling(false)
+            }}
+          >
+            구매
+          </button>
+          <button className="border border-black" onClick={() => modalClose3()}>
+            취소
+          </button>
+        </div>
+      </Modal>
+      {/* 가격 제안하기 모달 끝 */}
+
       <div className="h-[50%] w-full flex">
         <div className="w-[50%]">
           <img
@@ -96,24 +189,52 @@ const NftDetail = () => {
         <div className="w-[50%]">
           <p>DREAMMUNGS</p>
           <p>Tags</p>
-          <p>500 SSF</p>
+          <p>500 MUNG</p>
           <p>분양자</p>
           <p>Unique</p>
-          {myNft && myNft === true ? (
+
+          {}
+
+          {/* 본인 NFT 인지 확인 */}
+          {myNft === false ? (
             <div className="flex">
-              <button className="border border-black mr-3">즉시 구매가</button>
-              <button className="border border-black">가격 제안하기</button>
+              <button
+                className="border border-black mr-3"
+                onClick={() => {
+                  setOpen2(true)
+                }}
+              >
+                즉시 구매
+              </button>
+              <button
+                className="border border-black"
+                onClick={() => {
+                  setOpen3(true)
+                }}
+              >
+                가격 제안하기
+              </button>
             </div>
           ) : (
             <div className="flex">
-              {isSelling && isSelling === true ? (
+              {isSelling === true ? (
                 // 본인 NFT && 판매중인 경우
-                <button className="w-full border border-black">
+                <button
+                  className="w-full border border-black"
+                  onClick={() => {
+                    setOpen1(true)
+                  }}
+                >
                   판매 중지
                 </button>
               ) : (
                 // 본인 NFT && 판매 올리지 않았을 경우
-                <button className="w-full border border-black">
+                <button
+                  className="w-full border border-black"
+                  onClick={() => {
+                    setClickedSell(true)
+                  }}
+                >
                   판매 시작
                 </button>
               )}
@@ -128,7 +249,7 @@ const NftDetail = () => {
               <p className="text-center mb-1">즉시 구매가 설정</p>
               <div className="flex">
                 <input id="price" type="text" />
-                <label htmlFor="price">SSF</label>
+                <label htmlFor="price">MUNG</label>
               </div>
             </div>
 
@@ -153,9 +274,27 @@ const NftDetail = () => {
               </div>
             </div>
 
-            <button type="submit" className="mt-5 border border-black">
-              판매 등록
-            </button>
+            <div className="flex justify- center mt-5">
+              <button
+                type="submit"
+                className="border border-black mr-5"
+                onClick={() => {
+                  setClickedSell(false)
+                  setIsSelling(true)
+                }}
+              >
+                판매 등록
+              </button>
+
+              <button
+                className="border border-black"
+                onClick={() => {
+                  setClickedSell(false)
+                }}
+              >
+                취소
+              </button>
+            </div>
           </form>
         </div>
       ) : (
@@ -237,3 +376,8 @@ const NftDetail = () => {
 }
 
 export default NftDetail
+
+//
+// NFT 거래
+// NFT 발급
+// NFT
