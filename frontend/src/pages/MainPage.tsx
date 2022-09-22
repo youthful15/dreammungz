75377b25a239f0e947400a9dc8b3f6cfd4bbac3a
박종민 <qooktree@naver.com>
2022-09-22 16:40:10 +0900

    Design: Mainpage Carousel Test 1

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 import Carousel from "../components/carousel/Carousel"
-
+import CustomCarousel from "../components/carousel/CustomCarousel"
 export default function MainPage() {
   return (
     <div className="w-full h-full">
