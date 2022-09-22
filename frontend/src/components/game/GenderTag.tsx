8aa225a68854346e97824e90a005e840cb8773f0
songji <youthful_15@naver.com>
2022-09-22 19:46:06 +0900

    Fix: 충돌 내역 수정

--- frontend/src/components/game/GenderTag.tsx
+++ frontend/src/components/game/GenderTag.tsx
@@ -8,7 +8,6 @@ const color: { [index: string]: string } = {
   F: "bg-pink-200",
 }
 
-<<<<<<< HEAD
 export interface GenderType {
   name: string
 }
@@ -17,24 +16,10 @@ const GenderTag = ({ name }: GenderType) => {
   return (
     <div
       className={`text-black text-xs p-0.5 rounded-md m-0.5 w-6 text-center font-semibold shadow-xl border-white ${color[name]}`}
-=======
-export interface genderType {
-  name: string
-}
-
-const genderTag = ({ name }: genderType) => {
-  return (
-    <div
-      className={`text-black text-xs p-0.5 rounded-md m-0.5 w-5 text-center font-semibold ${color[name]}`}
->>>>>>> 5b8ef34d1a964d7f2023e35568f1dcfd80c46911
     >
       {stats[name]}
     </div>
   )
 }
 
-<<<<<<< HEAD
 export default GenderTag
-=======
-export default genderTag
->>>>>>> 5b8ef34d1a964d7f2023e35568f1dcfd80c46911
