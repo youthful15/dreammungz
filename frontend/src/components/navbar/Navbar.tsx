4b4029aaedfeaadbeee69255dc364db4b4c3e5f1
songji <youthful_15@naver.com>
2022-09-23 00:08:44 +0900

    Feat: 게임 시작 페이지 이동 api 추가

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react"
-import { Link, NavLink } from "react-router-dom"
+import { Link, useNavigate, NavLink } from "react-router-dom"
 import NavList from "./NavList"
 import memberAtom from "../../recoil/member/atom"
 import { useRecoilState } from "recoil"
 import { MUNGContract } from "../../utils/Web3Config"
+import { http } from "../../api/axios"
 
 const navItemStyle: string =
   "bg-brown-300  border rounded-lg shadow-sm cursor-pointer"
 
 const Navbar = () => {
+  const navigate = useNavigate()
   const [member] = useRecoilState(memberAtom)
-
   const [isLogin, setLogin] = useState(false) // 추후 Recoil을 사용하여  상태관리 할 것
 
   // balance Test 확인 완료
@@ -22,6 +23,23 @@ const Navbar = () => {
     console.log(balance)
   }
 
+  const startData = {
+    address: "0x1", // 차후 실제 address로 변경 필요
+    selection: "-1",
+  }
+
+  // 게임 시작 페이지로 이동 로직
+  function gameStart() {
+    console.log("address가 하드코딩 처리 되어있습니다.")
+    http.post(`game/info`, startData).then((res) => {
+      if (res.data.title) {
+        navigate("/game")
+      } else {
+        navigate("/start")
+      }
+    })
+  }
+
   return (
     <nav className="flex flex-col justify-between w-full h-full p-4 space-y-4 text-center">
       <ul>
@@ -31,9 +49,9 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink to="/start" className="flex justify-center w-full">
+          <button onClick={gameStart} className="flex justify-center w-full">
             <img src="/paw.png" alt="" className="" />
-          </NavLink>
+          </button>
         </li>
 
         <NavList />
