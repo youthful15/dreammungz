60b01e3d5e185ef14c657956b7f755367612c31a
안지영 <anji6978@naver.com>
2022-09-23 14:54:44 +0900

    Design: 필터 선택 화면 UI 변경

--- frontend/src/components/filter/FilterForm.tsx
+++ frontend/src/components/filter/FilterForm.tsx
@@ -8,82 +8,160 @@ import {
   gender_list,
   stat_list,
 } from "../../utils/gameWord"
+import Filter from "./Filter"
 
+const listStyle = "w-1/2 p-1 text-sm "
+const labelStyle =
+  "p-0.5  border border-gray-700 rounded-md bg-beige-300 peer-checked:bg-blue-300 w-full inline-block"
+const titleStyle = "p-1 font-bold   m-0.5 border-b-2 border-beige-100"
 type filterFormProp = {
   formHandler: (event: ChangeEvent<HTMLFormElement>) => void
+  filter: Filter
 }
-const FilterForm = ({ formHandler }: filterFormProp) => {
+const FilterForm = ({ formHandler, filter }: filterFormProp) => {
   return (
-    <div className="bg-white absolute z-10">
-      필터옵션
-      <form onChange={formHandler}>
-        <p>판매중인것만 보기</p>
-        <label>
-          <input type="checkbox" name="sell" />
-        </label>
-        <p>직업</p>
-        {job_list.map((item) => {
-          return (
-            <label key={item.eng}>
-              <input type="radio" name="job" value={item.eng} />
-              {item.kor}
-            </label>
-          )
-        })}
-        <p>모질</p>
-        {hair_list.map((item) => {
-          return (
-            <label key={item.eng}>
-              <input type="radio" name="hair" value={item.eng} />
-              {item.kor}
-            </label>
-          )
-        })}
-        <p>티어</p>
-        {tier_list.map((item) => {
-          return (
-            <label key={item.eng}>
-              <input type="radio" name="tier" value={item.eng} />
-              {item.kor}
-            </label>
-          )
-        })}
-        <p>성별</p>
-        {gender_list.map((item) => {
-          return (
-            <label key={item.eng}>
-              <input type="radio" name="gender" value={item.eng} />
-              {item.kor}
-            </label>
-          )
-        })}
-        <p>색상</p>
-        {color_list.map((item) => {
-          return (
-            <label key={item.eng}>
-              <input type="radio" name="color" value={item.eng} />
-              {item.kor}
-            </label>
-          )
-        })}
-        <p>얼굴</p>
-        {face_list.map((item) => {
-          return (
-            <label key={item.eng}>
-              <input type="radio" name="face" value={item.eng} />
-              {item.kor}
-            </label>
-          )
-        })}
-        <p>능력 </p>
-        {stat_list.map((item) => {
-          return (
-            <label key={item.eng}>
-              <input type="checkbox" name="status" value={item.eng} />
-              {item.kor}
-            </label>
-          )
-        })}
+    <div className="h-[700px] w-[230px] bg-brown-200  overflow-y-auto scrollbar-hide rounded-l-xl absolute left-[7px] z-10 top-[7px] p-1">
+      <form onChange={formHandler} className="w-full text-center ">
+        <p className={titleStyle}>직업</p>
+        <ul className="flex flex-wrap w-full">
+          {job_list.map((item) => {
+            return (
+              <li key={item.eng} className={listStyle}>
+                <input
+                  type="radio"
+                  name="job"
+                  id={item.eng}
+                  value={item.eng}
+                  className="hidden peer"
+                  defaultChecked={filter?.job === item.eng}
+                />
+                <label htmlFor={item.eng} className={labelStyle}>
+                  {item.kor}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+        <p className={titleStyle}>모질</p>
+        <ul className="flex">
+          {hair_list.map((item) => {
+            return (
+              <li key={item.eng} className={listStyle}>
+                <input
+                  type="radio"
+                  name="hair"
+                  id={item.eng}
+                  value={item.eng}
+                  className="hidden peer"
+                  defaultChecked={filter?.hair === item.eng}
+                />
+                <label htmlFor={item.eng} className={labelStyle}>
+                  {item.kor}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+        <p className={titleStyle}>티어</p>
+        <ul className="flex flex-wrap">
+          {tier_list.map((item) => {
+            return (
+              <li key={item.eng} className={listStyle}>
+                <input
+                  type="radio"
+                  name="tier"
+                  id={item.eng}
+                  value={item.eng}
+                  className="hidden peer"
+                  defaultChecked={filter?.tier === item.eng}
+                />
+                <label htmlFor={item.eng} className={labelStyle}>
+                  {item.kor}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+        <p className={titleStyle}>성별</p>
+        <ul className="flex flex-wrap">
+          {gender_list.map((item) => {
+            return (
+              <li key={item.eng} className={listStyle}>
+                <input
+                  type="radio"
+                  name="gender"
+                  id={item.eng}
+                  value={item.eng}
+                  className="hidden peer"
+                  defaultChecked={filter?.gender === item.eng}
+                />
+                <label htmlFor={item.eng} className={labelStyle}>
+                  {item.kor}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+        <p className={titleStyle}>색상</p>
+        <ul className="flex flex-wrap">
+          {color_list.map((item) => {
+            return (
+              <li key={item.eng} className={listStyle}>
+                <input
+                  type="radio"
+                  name="color"
+                  id={item.eng}
+                  value={item.eng}
+                  className="hidden peer"
+                  defaultChecked={filter?.color === item.eng}
+                />
+                <label htmlFor={item.eng} className={labelStyle}>
+                  {item.kor}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+        <p className={titleStyle}>얼굴</p>
+        <ul className="flex flex-wrap">
+          {face_list.map((item) => {
+            return (
+              <li key={item.eng} className={listStyle}>
+                <input
+                  type="radio"
+                  name="face"
+                  id={item.eng}
+                  value={item.eng}
+                  className="hidden peer"
+                  defaultChecked={filter?.face === item.eng}
+                />
+                <label htmlFor={item.eng} className={labelStyle}>
+                  {item.kor}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+        <p className={titleStyle}>능력 </p>
+        <ul className="flex flex-wrap">
+          {stat_list.map((item) => {
+            return (
+              <li key={item.eng} className={listStyle}>
+                <input
+                  type="checkbox"
+                  name="status"
+                  id={item.eng}
+                  value={item.eng}
+                  className="hidden peer"
+                  defaultChecked={filter?.status?.includes(item.eng)}
+                />
+                <label htmlFor={item.eng} className={labelStyle}>
+                  {item.kor}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
       </form>
     </div>
   )
