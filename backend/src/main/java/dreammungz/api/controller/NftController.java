3ce87a526fa44ee71cc1abd043994a15698e5153
seulgi <seulgi9834@naver.com>
2022-09-23 10:02:03 +0900

    Feat: 지갑 주소 기준 판매중이지 않고, 소유하고 있는 NFT 조회, Querydsl 추가

--- backend/src/main/java/dreammungz/api/controller/NftController.java
+++ backend/src/main/java/dreammungz/api/controller/NftController.java
@@ -1,5 +1,6 @@
 package dreammungz.api.controller;
 
+import dreammungz.api.dto.nft.MyNftResponse;
 import dreammungz.api.dto.nft.info.GameEndRequest;
 import dreammungz.api.dto.nft.info.GameEndResponse;
 import dreammungz.api.dto.nft.list.NftListRequest;
@@ -29,6 +30,13 @@ public class NftController {
         NftListResponse response = nftService.searchNft(nftListRequest);
         return new ResponseEntity<>(response, HttpStatus.OK);
 
+    }
+
+    @ApiOperation(value = "소유하고 있는 NFT 조회", notes = "판매중인 NFT를 제외한 소유한 NFT 정보를 제공한다.", response = MyNftResponse.class)
+    @GetMapping("/address/{address}")
+    public ResponseEntity<MyNftResponse> myNftList(@ApiParam(value = "지갑 주소", required = true) @PathVariable String address) {
+        MyNftResponse response = nftService.myNftList(address);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @ApiOperation(value = "엔딩 결과 조회", notes = "지갑 주소에 있는 게임 데이터를 조회해 엔딩 결과를 생성한다.", response = GameEndResponse.class)
