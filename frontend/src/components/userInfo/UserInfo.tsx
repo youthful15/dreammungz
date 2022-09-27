94c586b2c0055b416ac321c9e97ce7080e026d6b
안지영 <anji6978@naver.com>
2022-09-27 12:00:10 +0900

    Feat: 닉네임 변경 추가[S07P22A605S07P22A605-193 ]

--- frontend/src/components/userInfo/UserInfo.tsx
+++ frontend/src/components/userInfo/UserInfo.tsx
@@ -1,18 +1,57 @@
 import { useQuery } from "@tanstack/react-query"
-import { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import { getUserNickname } from "../../api/auth"
+import { http } from "../../api/axios"
 
-const originNick = "천재지영!!"
 const UserInfo = () => {
   const { address } = useParams()
+  const [newNickname, setNewNickname] = useState<string | null>(null)
   const [editing, setEditing] = useState(false)
-  const { data, error } = useQuery(["nickname", address], () =>
+  const [changedNick, setChangedNick] = useState<string | null>(null)
+  const [isOwner, setIsOwner] = useState<boolean | null>(null)
+  const { data, error } = useQuery(["nickname", address, changedNick], () =>
     getUserNickname(address!)
   )
   if (error) {
     console.log(error)
   }
+
+  useEffect(() => {
+    if (address) {
+      if (address === localStorage.getItem("publicAddress")) {
+        setIsOwner(true)
+      } else {
+        setIsOwner(false)
+      }
+    }
+  }, [address])
+
+  const onNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    setNewNickname(value)
+  }
+
+  const onNicknameSubmit = async () => {
+    if (!address) return
+    try {
+      const data = await http.put(`auth/info/nickname`, {
+        address,
+        nickname: newNickname,
+      })
+      if (data.status === 200) {
+        setChangedNick(newNickname)
+      }
+    } catch (e: any) {
+      const { data } = e.response
+      alert(`닉네임 수정 오류. ${data?.code} ${data?.message}`)
+    }
+  }
+  useEffect(() => {
+    if (changedNick) {
+      setEditing(false)
+    }
+  }, [changedNick])
 
   return (
     <>
@@ -21,30 +60,25 @@ const UserInfo = () => {
           <h1 className="text-2xl font-bold ">
             {data?.nickname}의 NFT 페이지{" "}
           </h1>
-          <button
-            className="p-1 ml-4 bg-white rounded-lg"
-            onClick={() => {
-              setEditing(true)
-            }}
-          >
-            ✏️
-          </button>
+          {isOwner !== null && isOwner && (
+            <button
+              className="p-1 ml-4 bg-white rounded-lg"
+              onClick={() => {
+                setEditing(true)
+              }}
+            >
+              ✏️
+            </button>
+          )}
         </div>
       ) : (
         <div className="space-x-2">
           <input
             type="text"
             defaultValue={data?.nickname}
-            onChange={(e) => {
-              console.log("닉넴변경")
-            }}
+            onChange={onNicknameChange}
           />
-          <button
-            className="border"
-            onClick={() => {
-              setEditing(false)
-            }}
-          >
+          <button className="border" onClick={onNicknameSubmit}>
             수정 하기{" "}
           </button>
           <button
