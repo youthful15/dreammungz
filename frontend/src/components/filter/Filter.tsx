a721f9fe1b52b8b544d4922dd9bc6d5d640e56e3
안지영 <anji6978@naver.com>
2022-09-29 00:18:56 +0900

    Factor: 선택된 필터 옵션 파일 분리

--- frontend/src/components/filter/Filter.tsx
+++ frontend/src/components/filter/Filter.tsx
@@ -1,6 +1,6 @@
-import React, { ChangeEvent, useState } from "react"
-import { NavigateOptions, useNavigate } from "react-router-dom"
-import findKOR from "../../utils/findKOR"
+import React, { ChangeEvent, useEffect, useState } from "react"
+import { NavigateOptions } from "react-router-dom"
+
 import FilterForm from "./FilterForm"
 
 interface Filter {
@@ -16,53 +16,18 @@ interface Filter {
   address: string | null
 }
 
-const buttonStyle = "p-1 m-0.5 border border-gray-700 rounded-md  h-8"
-export const SelectedFilters = ({
-  filter,
-  resetFilter,
-}: {
-  filter: Filter
-  resetFilter: () => void
-}) => {
-  const selected: string[] = Object.values(filter)
-    .filter(
-      (val) =>
-        typeof val !== "boolean" && val !== null && typeof val !== "number"
-    )
-    .flat()
-
-  // console.log(selected)
-
-  return (
-    <div className="flex flex-wrap overflow-x-auto">
-      {selected.map((value) => {
-        return (
-          <div className={buttonStyle} key={value}>
-            {findKOR(value)}
-          </div>
-        )
-      })}
-
-      {selected.length !== 0 && (
-        <button
-          className={`${buttonStyle} bg-white`}
-          onClick={() => {
-            resetFilter()
-          }}
-        >
-          필터 초기화
-        </button>
-      )}
-    </div>
-  )
-}
-
 interface FilterProp {
   filter: Filter
   showForm: boolean
   setFilter: (newQuery: Filter, options?: NavigateOptions | undefined) => void
+  selectedList: string[]
 }
-const Filter = ({ filter: origin, setFilter, showForm }: FilterProp) => {
+const Filter = ({
+  filter: origin,
+  setFilter,
+  showForm,
+  selectedList,
+}: FilterProp) => {
   const handleFormChange = (event: ChangeEvent<HTMLFormElement>) => {
     let form = event.currentTarget
     let formData = new FormData(form)
@@ -82,6 +47,10 @@ const Filter = ({ filter: origin, setFilter, showForm }: FilterProp) => {
     // console.log(JSON.stringify(filter))
     setFilter(filter, { replace: false })
   }
+  useEffect(() => {
+    const ele = document.getElementById("filter-form")
+    ele?.dispatchEvent(new Event("change", { bubbles: true }))
+  }, [origin])
 
   return (
     <div className="">
@@ -89,7 +58,11 @@ const Filter = ({ filter: origin, setFilter, showForm }: FilterProp) => {
         {/* <SelectedFilters filter={origin} resetFilter={resetFilter} /> */}
       </div>
       {showForm && (
-        <FilterForm formHandler={handleFormChange} filter={origin} />
+        <FilterForm
+          formHandler={handleFormChange}
+          filter={origin}
+          selectedList={selectedList}
+        />
       )}
     </div>
   )
