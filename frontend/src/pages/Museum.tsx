81bdbae874f0166c6c67d19091c7cf10aca94bf3
안지영 <anji6978@naver.com>
2022-09-29 21:57:30 +0900

    Feat: 로딩 화면 추가

--- frontend/src/pages/Museum.tsx
+++ frontend/src/pages/Museum.tsx
@@ -1,14 +1,16 @@
 import { useNavigate } from "react-router-dom"
+import Loading from "../components/museum/Loading/Loading"
 import Test from "../components/museum/Test"
 
 const Museum = () => {
   const navigate = useNavigate()
   return (
-    <div className="w-screen h-screen bg-white fixed top-0 left-0">
+    <div className="fixed top-0 left-0 w-screen h-screen bg-white">
       <div onClick={() => navigate(-1)} className="absolute z-30">
         뒤로가기
       </div>
       <Test />
+      <Loading />
     </div>
   )
 }
