232f54c849f806db4d5a55733aa37dad0b88cdb8
안지영 <anji6978@naver.com>
2022-09-30 10:56:03 +0900

    Design: nft 리스트 ui 크기 조절 (필터 선택창, 페이지네이션)

--- frontend/src/pages/NftListPage.tsx
+++ frontend/src/pages/NftListPage.tsx
@@ -2,7 +2,7 @@ import NftList from "../components/nftList/NftList"
 
 const NftListPage = () => {
   return (
-    <div className="h-full ">
+    <div className="h-full overflow-hidden">
       <h1 className="mb-3 text-2xl font-bold h-[10%]">
         드림멍즈의 모든 강아지들{" "}
       </h1>
