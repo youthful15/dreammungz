9d1e55ed7976ef3443f52e780f7a97d74121744a
박종민 <qooktree@naver.com>
2022-10-04 02:07:00 +0900

    Fix: Spinner Modal div 밖에 클릭시 modal close되게 수정

--- frontend/src/utils/web3.js
+++ frontend/src/utils/web3.js
@@ -12,11 +12,18 @@ export const pushGameStart = async (publicAddress, cost) => {
   const totalWeiValue = web3.utils
     .toBN(aWeiValue)
     .mul(web3.utils.toBN(bWeiValue))
-
-  await MUNGContract.methods
-    .approve(publicAddress, totalWeiValue)
-    .send({ from: publicAddress })
-  await MUNGContract.methods
-    .transferFrom(publicAddress, MUNGContractAddress, totalWeiValue)
-    .send({ from: publicAddress })
+  try {
+    await MUNGContract.methods
+      .approve(publicAddress, totalWeiValue)
+      .send({ from: publicAddress })
+    await MUNGContract.methods
+      .transferFrom(publicAddress, MUNGContractAddress, totalWeiValue)
+      .send({ from: publicAddress })
+  } catch {
+    await setTrade((prev) => {
+      const variable = { ...prev }
+      variable.modalOpen6 = false
+      return { ...variable }
+    })
+  }
 }
