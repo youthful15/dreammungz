3416a6dcbafda5bbd7c1831e9d72788de352d849
박종민 <qooktree@naver.com>
2022-10-04 14:51:09 +0900

    Fix: 인터프리터 주석 처리

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -12,23 +12,23 @@ function Instance() {
   })
 
   // 헤더에 토큰 등록
-  instance.interceptors.request.use(
-    (config: any) => {
-      // token 만료 시 login Page 로
-      // TokenExpireCheck()
-      if (false) {
-        // 로그아웃
-        localStorage.clear()
-        window.location.replace("/login")
-      } else {
-        config.headers["token"] = localStorage.getItem("token")
-        return config
-      }
-    },
-    (err) => {
-      return Promise.reject(err)
-    }
-  )
+  // instance.interceptors.request.use(
+  //   (config: any) => {
+  //     // token 만료 시 login Page 로
+  //     // TokenExpireCheck()
+  //     if (false) {
+  //       // 로그아웃
+  //       localStorage.clear()
+  //       window.location.replace("/login")
+  //     } else {
+  //       config.headers["token"] = localStorage.getItem("token")
+  //       return config
+  //     }
+  //   },
+  //   (err) => {
+  //     return Promise.reject(err)
+  //   }
+  // )
 
   return instance
 }
