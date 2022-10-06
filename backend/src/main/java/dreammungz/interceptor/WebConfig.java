3f3bdfc6432208662c8c43e64902631ae6853469
황승주 <las1260@naver.com>
2022-10-06 13:41:07 +0900

    Feat: 인터셉터 설정

--- backend/src/main/java/dreammungz/interceptor/WebConfig.java
+++ backend/src/main/java/dreammungz/interceptor/WebConfig.java
@@ -2,6 +2,7 @@ package dreammungz.interceptor;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 */
 
 @Configuration
-//@EnableWebMvc
+@EnableWebMvc
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
