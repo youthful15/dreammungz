0f3e1f5460aca542cdb3076966cfccaa08ec789b
황승주 <las1260@naver.com>
2022-10-06 23:06:14 +0900

    Fix: swagger 작동하게 수정

--- backend/src/main/java/dreammungz/interceptor/WebConfig.java
+++ backend/src/main/java/dreammungz/interceptor/WebConfig.java
@@ -2,7 +2,6 @@ package dreammungz.interceptor;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 */
 
 @Configuration
-@EnableWebMvc
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
